b3355586f6fb9386c45211e224f124d7b38d618b
MeDeity <546024423@qq.com>
2018-01-09 20:24:53 +0800

    (+)新增  日志记录 log4j的支持

--- src/main/java/com/deity/controller/TestController.java
+++ src/main/java/com/deity/controller/TestController.java
@@ -1,18 +1,23 @@
 package com.deity.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 测试用
+ * 其中 @RestController = @Controller+@ResponseBody
  * Created by Deity on 2017/12/28.
  */
 @RestController
 @RequestMapping("/test")
 public class TestController {
+    private static Logger logger = Logger.getLogger(TestController.class);
 
-    @RequestMapping("/index")
+    @RequestMapping(value = "/index",method = RequestMethod.GET)
     public String testIndex(){
+        logger.info("invoke testIndex");
         return "this is index str";
     }
 }
